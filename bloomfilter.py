14703739edfba44fb89c7c9715f5df4ec8be4f40
glace23 <vikash23galce@gmail.com>
2021-10-28 02:11:46 +0800

    update

--- bloomfilter.py
+++ bloomfilter.py
@@ -78,10 +78,10 @@ class BloomFilter:
         for i in range(0, self.hash_count):
             # Initialize hash value for first loop
             if i == 0:
-                hash_item = MD5.new(data=bytearray(item, encoding="ISO-8859-1"))
+                hash_item = MD5.new(data=bytearray(item, encoding=self.encoding))
             # Update hash value for subsequent loop
             else:
-                hash_item.update(data=bytearray(item, encoding="ISO-8859-1"))
+                hash_item.update(data=bytearray(item, encoding=self.encoding))
 
             # Get index for bit array
             # Digest hex hash value and convert to decimal
@@ -103,10 +103,10 @@ class BloomFilter:
         for i in range(0, self.hash_count):
             # Initialize hash value for first loop
             if i == 0:
-                hash_item = MD5.new(data=bytearray(item, encoding="ISO-8859-1"))
+                hash_item = MD5.new(data=bytearray(item, encoding=self.encoding))
             # Update hash value for subsequent loop
             else:
-                hash_item.update(data=bytearray(item, encoding="ISO-8859-1"))
+                hash_item.update(data=bytearray(item, encoding=self.encoding))
 
             # Get index for bit array
             # Digest hex hash value and convert to decimal
